51fe5a9fbc9b51aa860a4ad8975f36e86723606e
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-06-14 11:06:00 -0500

    directorio de componentes

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+// para tratar esta clase como componente se requiere escribir el siguiente decorador
+import { Component } from '@angular/core';
+
+// configurar componente
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html'
+})
+export class HeaderComponent{
+
+}
+
