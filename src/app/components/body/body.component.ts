05791fbebbc909d5f3934565aaab336413d505a7
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-06-17 11:55:25 -0500

    finalizacion de poryecto de introduccion a angular

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -5,4 +5,15 @@ import { Component } from '@angular/core';
     templateUrl: './body.component.html'
 })
 
-export class BodyComponent{}
+export class BodyComponent {
+
+    // propiedades
+    mostrar = true;
+
+    frase: any = {
+        mensaje : 'Un gran poder require una gran responsabilidad',
+        autor: 'Ben parker'
+    };
+
+    personajes: string[] = ['spiderman', 'venom', 'Dr. octopus'];
+}
